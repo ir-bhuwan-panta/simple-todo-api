9d93031ae482c13dd69352c5d7b0231e339dcc9c
Bhuwan Panta <bhuwan.panta@invisirisk.com>
2025-05-28 16:57:15 +0545

    Update get_log.sh

--- .github/actions/my-action/get_log.sh
+++ .github/actions/my-action/get_log.sh
@@ -3,8 +3,9 @@ if [[ "$MODE" != "analyze" ]]; then
     # Add your normal action logic here
     return 0
 fi
+
 curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/ir-bhuwan-panta/$GITHUB_REPOSITORY/actions/jobs/$PSE_JOB_ID/logs
+  https://api.github.com/repos/$GITHUB_REPOSITORY/actions/jobs/$PSE_JOB_ID/logs
